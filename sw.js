d1ffe7d0ed4dc6e5cc6b85ce10fa322f1381b002
GuillePrograma94 <guilleprograma94@gmail.com>
2025-10-11 10:08:46 +0200

    Añadimos carga en background

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Maneja cache y funcionamiento offline
  */
 
-const CACHE_NAME = 'scan-as-you-shop-v6';
+const CACHE_NAME = 'scan-as-you-shop-v7';
 const urlsToCache = [
     '/',
     '/index.html',
